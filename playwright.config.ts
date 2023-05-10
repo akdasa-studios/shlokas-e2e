2c0a57dbe1a5795895e7420a27134da5608c0357
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-10 08:54:57 +0200

    fix: forbidOnly for test purposes

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   timeout: 60 * 1000,
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : (process.env.CI ? 1 : undefined),
   outputDir: './output/results',
