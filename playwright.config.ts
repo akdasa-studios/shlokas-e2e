ac263aeca79bd965d15aa8e456c2104792e269c8
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-10 18:17:25 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   // forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : (process.env.CI ? 1 : undefined),
   outputDir: './output/results',
   reporter: [
