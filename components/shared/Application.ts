9003f1e921c798d1468b0272ac870b35ca2ec06e
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-12-17 14:44:25 +0100

    feat: tutorial scenarios (#5)

--- components/shared/Application.ts
+++ components/shared/Application.ts
@@ -1,10 +1,12 @@
 import { InboxDeckPage, ReviewDeckPage } from '@components/decks'
 import { LibraryPage, VerseDetailsPage } from '@components/library'
 import { TabsBar } from '@components/shared'
+import { TutorialOverlay } from '@components/tutorial'
 import { Page } from '@playwright/test'
 
 export interface ApplicationParams {
   tutorialEnabled?: boolean,
+  tutorialStep?: number,
   libraryLastSyncDate?: number,
   autoSyncOnLogin?: boolean,
   reviewCardsInRandomOrder?: boolean,
@@ -17,26 +19,41 @@ export class Application {
   public readonly tabs: TabsBar
   public readonly inboxDeck: InboxDeckPage
   public readonly reviewDeck: ReviewDeckPage
+  public readonly tutorial: TutorialOverlay
 
-  constructor(public readonly page: Page) {
+  constructor(
+    public readonly page: Page
+  ) {
     this.library = new LibraryPage(page)
     this.verseDetails = new VerseDetailsPage(page)
     this.inboxDeck = new InboxDeckPage(page)
     this.tabs = new TabsBar(page)
     this.reviewDeck = new ReviewDeckPage(page)
+    this.tutorial = new TutorialOverlay(page.getByTestId("tutorial-overlay"))
   }
 
-  async open(date: Date = undefined) {
-    const params: ApplicationParams = {
+  async open(
+    params: ApplicationParams = {}
+  ) {
+    const defaultParams: ApplicationParams = {
       tutorialEnabled: false,
       libraryLastSyncDate: 9999999999999,
       reviewCardsInRandomOrder: false,
     }
-    if (date) { params.date = date }
-    await this.goto('/home/library', params)
+    await this.goto(
+      '/home/library',
+       Object.assign({}, defaultParams, params)
+    )
   }
 
-  async goto(url: string, params?: ApplicationParams) {
+  async reload() {
+    await this.page.reload()
+  }
+
+  async goto(
+    url: string,
+    params?: ApplicationParams
+  ) {
     if (params) {
       // @ts-ignore
       const queryString = new URLSearchParams(params).toString()
---
+++ tests/tutorial/Tutorial.Full.spec.ts
@@ -0,0 +1,28 @@
+import { Application } from '@components/shared'
+import { expect, test } from '@playwright/test'
+
+
+test.describe('Tutorial :: Full', () => {
+  let app: Application
+
+  test.beforeEach(async ({ page }) => {
+    app = new Application(page)
+    await app.open({ tutorialEnabled: true })
+  })
+
+  test('User is able to go throught all tutorial', async () => {
+    await app.tutorial.button("yes").click()
+
+    await app.tutorial.card(2).waitFor()
+    await app.library.verse("BG 1.1").click()
+    await app.verseDetails.addButton.click()
+    await app.tabs.inboxTab.click()
+
+    await app.tutorial.card(6).waitFor()
+    await app.inboxDeck.swipeCardLeft()
+
+    // TODO: TBD later...
+
+  })
+
+})
